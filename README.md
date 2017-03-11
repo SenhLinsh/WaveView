10e7ea4c2ba1810ab1a0004ef4f7a4ba5861fb4e
Senh Linsh <lyuanxiang0812@126.com>
2017-03-11 17:57:23 +0800

    完善 README

--- README.md
+++ README.md
@@ -11,3 +11,39 @@ UI说, 我给几张图, 你自己挪吧.<br/>
 于是, 波浪流动的动态效果的自定义控件应运而生.<br/>
 <br/>
 希望有人会喜欢~<br/>
+
+## How To Use
+
+XML:
+
+```
+    <com.linsh.waveview.WaveView
+        android:id="@+id/waveview"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"/>
+```
+
+Activity:
+
+```
+        int color = Color.parseColor("#55303F9F");
+        Wave wave1 = new Wave(1080, 30, 5, 45, color);
+        Wave wave2 = new Wave(1620, 26, -6, 50, color);
+        Wave wave3 = new Wave(2080, 3, 3, 300, color);
+        waveView.addWave(wave1);
+        waveView.addWave(wave2);
+        waveView.addWave(wave3);
+```
+
+or
+
+```
+        LinearGradient shader = new LinearGradient(0, 0, 0, 1920, Color.parseColor("#303F9F"), Color.parseColor("#553F51B5"), Shader.TileMode.CLAMP);
+        PercentWave wave1 = new PercentWave(1.50f, 0.03f, 0.018f, 0.11f, shader);
+        PercentWave wave2 = new PercentWave(1.28f, 0.02f, -0.02f, 0.12f, shader);
+        PercentWave wave3 = new PercentWave(2.08f, 0.01f, 0.01f, 0.20f, shader);
+        waveView.addWave(wave1);
+        waveView.addWave(wave2);
+        waveView.addWave(wave3);
+```
+<br/>
--- demo/src/main/java/com/linsh/demo/MainActivity.java
+++ demo/src/main/java/com/linsh/demo/MainActivity.java
@@ -1,12 +1,10 @@
 package com.linsh.demo;
 
 import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.linsh.waveview.PercentWave;
+import com.linsh.waveview.Wave;
 import com.linsh.waveview.WaveView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +16,20 @@ public class MainActivity extends AppCompatActivity {
         WaveView waveView = (WaveView) findViewById(R.id.waveview);
         waveView.setOrientation(WaveView.Orientation.DOWN);
 
-        LinearGradient shader = new LinearGradient(0, 0, 0, 1920, Color.parseColor("#303F9F"), Color.parseColor("#553F51B5"), Shader.TileMode.CLAMP);
-        PercentWave wave1 = new PercentWave(1.50f, 0.03f, 0.018f, 0.11f, shader);
-        PercentWave wave2 = new PercentWave(1.28f, 0.02f, -0.02f, 0.12f, shader);
-        PercentWave wave3 = new PercentWave(2.08f, 0.01f, 0.01f, 0.20f, shader);
+        int color = Color.parseColor("#55303F9F");
+        Wave wave1 = new Wave(1080, 30, 5, 45, color);
+        Wave wave2 = new Wave(1620, 26, -6, 50, color);
+        Wave wave3 = new Wave(2080, 3, 3, 300, color);
         waveView.addWave(wave1);
         waveView.addWave(wave2);
         waveView.addWave(wave3);
+
+//        LinearGradient shader = new LinearGradient(0, 0, 0, 1920, Color.parseColor("#303F9F"), Color.parseColor("#553F51B5"), Shader.TileMode.CLAMP);
+//        PercentWave wave1 = new PercentWave(1.50f, 0.03f, 0.018f, 0.11f, shader);
+//        PercentWave wave2 = new PercentWave(1.28f, 0.02f, -0.02f, 0.12f, shader);
+//        PercentWave wave3 = new PercentWave(2.08f, 0.01f, 0.01f, 0.20f, shader);
+//        waveView.addWave(wave1);
+//        waveView.addWave(wave2);
+//        waveView.addWave(wave3);
     }
 }
