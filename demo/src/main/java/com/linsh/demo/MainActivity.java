68620c348bede2da89443222e2b46408afe668ab
Senh Linsh <lyuanxiang0812@126.com>
2017-03-11 17:41:54 +0800

    完善 README

--- demo/src/main/java/com/linsh/demo/MainActivity.java
+++ demo/src/main/java/com/linsh/demo/MainActivity.java
@@ -18,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         WaveView waveView = (WaveView) findViewById(R.id.waveview);
         waveView.setOrientation(WaveView.Orientation.DOWN);
 
-        LinearGradient shader = new LinearGradient(0, 0, 0, 600, Color.parseColor("#303F9F"), Color.parseColor("#773F51B5"), Shader.TileMode.CLAMP);
-        PercentWave wave1 = new PercentWave(1.50f, 0.03f, 0.018f, 0.26f, shader);
-        PercentWave wave2 = new PercentWave(1.28f, 0.02f, -0.02f, 0.28f, shader);
-        PercentWave wave3 = new PercentWave(2.08f, 0.01f, 0.01f, 0.49f, shader);
+        LinearGradient shader = new LinearGradient(0, 0, 0, 1920, Color.parseColor("#303F9F"), Color.parseColor("#553F51B5"), Shader.TileMode.CLAMP);
+        PercentWave wave1 = new PercentWave(1.50f, 0.03f, 0.018f, 0.11f, shader);
+        PercentWave wave2 = new PercentWave(1.28f, 0.02f, -0.02f, 0.12f, shader);
+        PercentWave wave3 = new PercentWave(2.08f, 0.01f, 0.01f, 0.20f, shader);
         waveView.addWave(wave1);
         waveView.addWave(wave2);
         waveView.addWave(wave3);
